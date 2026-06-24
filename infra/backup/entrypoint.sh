d08600752a3c31da9ebc7ddcb89f4d021432da6a
Angel226m <2201010137@undc.edu.pe>
2026-06-23 19:50:43 -0500

    implementacion de  dhparam.pem en tu servidor

--- infra/backup/entrypoint.sh
+++ infra/backup/entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 printenv | grep -v "no_proxy\|HOME\|PWD\|TERM\|SHLVL\|_\|PATH" > /etc/environment
 
-echo "# Backup diario a las 4:00 PM hora Peru (UTC-5 = 21:00 UTC)" > /var/spool/cron/crontabs/root
-echo "0 21 * * * /usr/local/bin/backup.sh >> /var/log/backup.log 2>&1" >> /var/spool/cron/crontabs/root
+echo "# Backup diario a las 7:55 PM hora Peru (UTC-5 = 00:55 UTC)" > /var/spool/cron/crontabs/root
+echo "55 0 * * * /usr/local/bin/backup.sh >> /var/log/backup.log 2>&1" >> /var/spool/cron/crontabs/root
 chmod 0644 /var/spool/cron/crontabs/root
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup scheduler iniciado"
-echo "  Horario: 4:00 PM Peru (21:00 UTC) todos los dias"
+echo "  Horario: 7:55 PM Peru (00:55 UTC) todos los dias"
 echo "  Bucket:  $B2_BUCKET"
 echo "  Retencion: ${RETENTION_DAYS:-10} dias"
 
